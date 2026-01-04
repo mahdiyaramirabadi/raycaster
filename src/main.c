3d92abab0b3a8ff976bfe5f216836ebd58c7f49b
Mahdiyar Amirabadi <mahdiyaramirabadi4@gmail.com>
2026-01-04 17:25:33 +0330

    edit map with mouse and change game mode with M

--- src/main.c
+++ src/main.c
@@ -25,6 +25,15 @@ int map[20][20] = {
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+const int screenWidth = 1200;
+const int screenHeight = 800;
+const int TILE_SIZE = 32;
+const int MAP_WIDTH = 20;
+const int MAP_HEIGHT = 20;
+const int MINI_TILE_SIZE = 10;
+const int padx = (screenWidth - TILE_SIZE * MAP_WIDTH) / 2,
+          pady = (screenHeight - TILE_SIZE * MAP_HEIGHT) / 2;
+const int map_padx = 950, map_pady = 10;
 
 Player RotateM(Player player, float rotSpeed, int d) {
     float OldDirX = player.dir.x;
@@ -47,20 +56,22 @@ int CheckTargetPos(float posX, float posY) {
     return 1;
 }
 
+void ModifyMap(int c, int posX, int posY) {
+    int x = (posX - padx) / TILE_SIZE;
+    int y = (posY - pady) / TILE_SIZE;
+    printf("%d %d %d\n", x, y, c);
+    if (x > 0 && x < MAP_WIDTH - 1 && y > 0 && y < MAP_HEIGHT - 1)
+        map[y][x] = c;
+}
+
 int main(void) {
-    const int screenWidth = 1200;
-    const int screenHeight = 800;
-    const int TILE_SIZE = 32;
-    const int MAP_WIDTH = 20;
-    const int MAP_HEIGHT = 20;
-    const int MINI_TILE_SIZE = 10;
-    const int padx = 950, pady = 10;
+    int game_mode = 0;
     Player player;
     player.pos.x = 1 * TILE_SIZE;
     player.pos.y = 1 * TILE_SIZE;
     player.map_pos.x = 1 * MINI_TILE_SIZE;
     player.map_pos.y = 1 * MINI_TILE_SIZE;
-    player.dir.x = -1.0;
+    player.dir.x = 1.0;
     player.dir.y = 0.0;
     player.plane.x = 0.0;
     player.plane.y = 0.66;
@@ -68,6 +79,19 @@ int main(void) {
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
+
+        // set game mode
+        if (IsKeyPressed(KEY_M)) {
+            game_mode = game_mode == 1 ? 0 : 1;
+        }
+        if (game_mode == 0) {
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                ModifyMap(1, GetMouseX(), GetMouseY());
+            }
+            if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+                ModifyMap(0, GetMouseX(), GetMouseY());
+            }
+        }
         float moveSpeed = 100.0 * dt;
         float rotSpeed = 5.0 * dt;
         if (IsKeyDown(KEY_W)) {
@@ -107,103 +131,130 @@ int main(void) {
             }
         }
         if (IsKeyDown(KEY_RIGHT)) {
-            player = RotateM(player, rotSpeed, -1);
+            player = RotateM(player, rotSpeed, 1);
         }
         if (IsKeyDown(KEY_LEFT)) {
-            player = RotateM(player, rotSpeed, 1);
+            player = RotateM(player, rotSpeed, -1);
         }
         BeginDrawing();
         ClearBackground(BLACK);
-
-        for (int x = 0; x < screenWidth; x++) {
-            double cameraX = 2 * (x / (double)screenWidth) - 1;
-            double rayDirX = player.dir.x + player.plane.x * cameraX;
-            double rayDirY = player.dir.y + player.plane.y * cameraX;
-
-            int mapX = floor(player.pos.x / TILE_SIZE),
-                mapY = floor(player.pos.y / TILE_SIZE);
-            double posX = player.pos.x / TILE_SIZE,
-                   posY = player.pos.y / TILE_SIZE;
-
-            double sideDistX, sideDistY;
-
-            double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-            double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-            double perpWallDist;
-
-            int stepX, stepY;
-
-            // DDA
-            int hit = 0;
-            int side;
-
-            if (rayDirX < 0) {
-                stepX = -1;
-                sideDistX = (posX - mapX) * deltaDistX;
-            } else {
-                stepX = 1;
-                sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-            }
-            if (rayDirY < 0) {
-                stepY = -1;
-                sideDistY = (posY - mapY) * deltaDistY;
-            } else {
-                stepY = 1;
-                sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-            }
-
-            while (!hit) {
-                if (sideDistX < sideDistY) {
-                    sideDistX += deltaDistX;
-                    mapX += stepX;
-                    side = 0;
-                } else {
-                    sideDistY += deltaDistY;
-                    mapY += stepY;
-                    side = 1;
+        if (game_mode == 0) {
+            DrawText("Save map(F5)", 200, 30, 20, WHITE);
+            DrawText("Reload map(F9)", 900, 30, 20, WHITE);
+            for (int y = 0; y < MAP_HEIGHT; y++) {
+                for (int x = 0; x < MAP_WIDTH; x++) {
+                    int screenX = padx + x * TILE_SIZE;
+                    int screenY = pady + y * TILE_SIZE;
+                    if (map[y][x] > 0) {
+                        DrawRectangle(screenX, screenY, TILE_SIZE, TILE_SIZE,
+                                      WHITE);
+                    } else {
+                        DrawRectangle(screenX, screenY, TILE_SIZE, TILE_SIZE,
+                                      BLACK);
+                    }
+                    DrawRectangleLines(screenX, screenY, TILE_SIZE, TILE_SIZE,
+                                       DARKGRAY);
                 }
-                if (map[mapY][mapX]) hit = 1;
             }
+            DrawCircle(padx + player.pos.x, pady + player.pos.y, 20, RED);
+            DrawLine(padx + player.pos.x, pady + player.pos.y,
+                     padx + player.pos.x + 40 * player.dir.x,
+                     pady + player.pos.y + 40 * player.dir.y, BLUE);
 
-            if (side == 0)
-                perpWallDist = (sideDistX - deltaDistX);
-            else
-                perpWallDist = (sideDistY - deltaDistY);
+        } else if (game_mode == 1) {
+            printf("%d\n", game_mode);
+            for (int x = 0; x < screenWidth; x++) {
+                double cameraX = 2 * (x / (double)screenWidth) - 1;
+                double rayDirX = player.dir.x + player.plane.x * cameraX;
+                double rayDirY = player.dir.y + player.plane.y * cameraX;
 
-            int lineHeight = (int)(screenHeight / perpWallDist);
+                int mapX = floor(player.pos.x / TILE_SIZE),
+                    mapY = floor(player.pos.y / TILE_SIZE);
+                double posX = player.pos.x / TILE_SIZE,
+                       posY = player.pos.y / TILE_SIZE;
 
-            int drawStart = screenHeight / 2 - lineHeight / 2;
-            if (drawStart < 0) drawStart = 0;
-            int drawEnd = screenHeight / 2 + lineHeight / 2;
-            if (drawEnd > screenHeight) drawEnd = screenHeight - 1;
-            Color color = GRAY;
-            if (side == 1) color = DARKGRAY;
-            DrawLine(x, 0, x, drawStart, SKYBLUE);
-            DrawLine(x, drawStart, x, drawEnd, color);
-            DrawLine(x, drawEnd, x, screenHeight, BLACK);
-        }
-        for (int y = 0; y < MAP_HEIGHT; y++) {
-            for (int x = 0; x < MAP_WIDTH; x++) {
-                if (map[y][x]) {
-                    DrawRectangle(padx + x * MINI_TILE_SIZE,
-                                  pady + y * MINI_TILE_SIZE, MINI_TILE_SIZE,
-                                  MINI_TILE_SIZE, BLACK);
+                double sideDistX, sideDistY;
+
+                double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
+                double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
+                double perpWallDist;
+
+                int stepX, stepY;
+
+                // DDA
+                int hit = 0;
+                int side;
+
+                if (rayDirX < 0) {
+                    stepX = -1;
+                    sideDistX = (posX - mapX) * deltaDistX;
                 } else {
-                    DrawRectangle(padx + x * MINI_TILE_SIZE,
-                                  pady + y * MINI_TILE_SIZE, MINI_TILE_SIZE,
-                                  MINI_TILE_SIZE, WHITE);
+                    stepX = 1;
+                    sideDistX = (mapX + 1.0 - posX) * deltaDistX;
                 }
-                DrawRectangleLines(padx + x * MINI_TILE_SIZE,
-                                   pady + y * MINI_TILE_SIZE, MINI_TILE_SIZE,
-                                   MINI_TILE_SIZE, DARKGRAY);
-            }
-        }
-        // printf("%f , %f\n",player.pos.x , player.pos.y);
+                if (rayDirY < 0) {
+                    stepY = -1;
+                    sideDistY = (posY - mapY) * deltaDistY;
+                } else {
+                    stepY = 1;
+                    sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+                }
 
-        DrawCircle(padx + player.map_pos.x, pady + player.map_pos.y, 4, RED);
-        DrawLine(padx + player.map_pos.x, pady + player.map_pos.y,
-                 padx + player.map_pos.x + 10 * player.dir.x,
-                 pady + player.map_pos.y + 10 * player.dir.y, RED);
+                while (!hit) {
+                    if (sideDistX < sideDistY) {
+                        sideDistX += deltaDistX;
+                        mapX += stepX;
+                        side = 0;
+                    } else {
+                        sideDistY += deltaDistY;
+                        mapY += stepY;
+                        side = 1;
+                    }
+                    if (map[mapY][mapX]) hit = 1;
+                }
+
+                if (side == 0)
+                    perpWallDist = (sideDistX - deltaDistX);
+                else
+                    perpWallDist = (sideDistY - deltaDistY);
+
+                int lineHeight = (int)(screenHeight / perpWallDist);
+
+                int drawStart = screenHeight / 2 - lineHeight / 2;
+                if (drawStart < 0) drawStart = 0;
+                int drawEnd = screenHeight / 2 + lineHeight / 2;
+                if (drawEnd > screenHeight) drawEnd = screenHeight - 1;
+                Color color = GRAY;
+                if (side == 1) color = DARKGRAY;
+                DrawLine(x, 0, x, drawStart, SKYBLUE);
+                DrawLine(x, drawStart, x, drawEnd, color);
+                DrawLine(x, drawEnd, x, screenHeight, BLACK);
+            }
+            for (int y = 0; y < MAP_HEIGHT; y++) {
+                for (int x = 0; x < MAP_WIDTH; x++) {
+                    if (map[y][x]) {
+                        DrawRectangle(map_padx + x * MINI_TILE_SIZE,
+                                      map_pady + y * MINI_TILE_SIZE,
+                                      MINI_TILE_SIZE, MINI_TILE_SIZE, BLACK);
+                    } else {
+                        DrawRectangle(map_padx + x * MINI_TILE_SIZE,
+                                      map_pady + y * MINI_TILE_SIZE,
+                                      MINI_TILE_SIZE, MINI_TILE_SIZE, WHITE);
+                    }
+                    DrawRectangleLines(map_padx + x * MINI_TILE_SIZE,
+                                       map_pady + y * MINI_TILE_SIZE,
+                                       MINI_TILE_SIZE, MINI_TILE_SIZE,
+                                       DARKGRAY);
+                }
+            }
+            // printf("%f , %f\n",player.pos.x , player.pos.y);
+
+            DrawCircle(map_padx + player.map_pos.x, map_pady + player.map_pos.y,
+                       4, RED);
+            DrawLine(map_padx + player.map_pos.x, map_pady + player.map_pos.y,
+                     map_padx + player.map_pos.x + 10 * player.dir.x,
+                     map_pady + player.map_pos.y + 10 * player.dir.y, RED);
+        }
         EndDrawing();
     }
     CloseWindow();
